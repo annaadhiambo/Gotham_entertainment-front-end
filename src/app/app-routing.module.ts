3f20b7d6a8b6b3a35c5737e264dd80368521970f
annaadhiambo <annaadhiambo01@gmail.com>
2020-11-05 07:01:50 +0300

    src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { BodyPageComponent } from './body-page.component';
+// import { LandingPageComponent } from './landing-page.component';
+// import { LoginFormComponent } from './login-form.component';
+// import { RegisterComponent } from './register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // {path:"", component: BodyPageComponent}
+  // {path:"", component: LandingPageComponent}
+  // {path:"login-form", component: LoginFormComponent}
+  // {path:"register", component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
