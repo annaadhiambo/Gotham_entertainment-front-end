a053ad011d7fd2870b476f4c5f1129df4c3f3c70
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-06 09:52:50 +0300

    add a routefor movie detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { BodyPageComponent } from './body-page/body-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 
 const routes: Routes = [
-  { path: "", component: BodyPageComponent },
-  { path: "", component: LandingPageComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  { path: "home", component: BodyPageComponent },
+  { path: "home/:id", component: MovieDetailComponent },
+  { path: "home", component: LandingPageComponent },
+  { path: "", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+
 ];
 
 @NgModule({
